26bd426ed7ccbfbdb1252f4f6754df694fda4dec
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-08-22 22:34:05 +0530

    all files added

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,14 @@
 
 cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
 mkdir /app
-
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-
 cd /app
 npm install
 
