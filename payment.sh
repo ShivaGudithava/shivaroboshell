3feb46ea169a096c7fa0300677ec5a31508a4395
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-09-08 23:12:55 +0530

    schema setup

--- payment.sh
+++ payment.sh
@@ -2,8 +2,8 @@ component=payment
 source common.sh
 rabbitmq_app_pasword=$1
 if [ -z "${rabbitmq_app_password}" ]; then
-   echo Input Rabbit AppUser Password Missing
+   echo Input RabbitMQ AppUser Password Missing
    exit 1
-   fi
+ fi
 func_python
 
